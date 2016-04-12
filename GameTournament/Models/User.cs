cbb1f9ca052f1a6bb17afc1e54a08f365b675d66
Dennis Kievits <denkievits@gmail.com>
2016-04-12 10:16:01 +0200

    Added last files

---
+++ GameTournament/Models/User.cs
@@ -0,0 +1,10 @@
+﻿namespace GameTournament.Models
+{
+	public class User
+	{
+		public int UserId { get; set; }
+		public string Firstname { get; set; }
+		public string Lastname { get; set; }
+		public int Studentnumber { get; set; }
+	}
+}
